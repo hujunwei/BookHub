ca35bc25466f01d9227887d8de08b0b91a29dba2
hujunwei <junweihu@Junweis-MacBook-Pro.local>
2014-09-14 14:25:09 -0400

    About to finish
    Only short of delete and edit

--- BookHub/app/controllers/microposts_controller.rb
+++ BookHub/app/controllers/microposts_controller.rb
@@ -7,7 +7,7 @@ class MicropostsController < ApplicationController
   def create
   	@micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:success] = "Micropost created!"
+      flash[:success] = "Books added!"
       redirect_to root_url
     else
       render 'static_pages/home'
